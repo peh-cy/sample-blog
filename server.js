c52c787f30eda6d76aaf42b0b273011cfc48db67
peh-cy <pehchowyoke@yahoo.com.sg>
2025-05-14 03:54:46 +0000

    added weather

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());       
 app.use(express.urlencoded({extended: true}));
 
+// add this snippet after "var express = require('express')"
+var axios = require('axios');
+
 // Needed for Prisma to connect to database
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
@@ -99,6 +102,18 @@ app.post("/delete/:id", async (req, res) => {
   app.get('/demo', function(req, res) {
   res.render('pages/demo');
 });
+// add this snippet before 
+app.get('/weather', async (req, res) => {
+    try {
+      const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
+      res.render('pages/weather', { weather: response.data });
+    } catch (error) {
+      console.error(error);
+      res.send('Error fetching weather data');
+    }
+  });
+  
+
 
 // Tells the app which port to run on
 app.listen(8080);
